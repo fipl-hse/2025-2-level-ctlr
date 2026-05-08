055d28e04bb70a26532af43d5d07c00d995a8597
anna_kaizerova <kaizerova.a@yandex.ru>
2026-05-08 11:21:05 +0300

    fixing code style

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -287,28 +287,26 @@ class Crawler:
 
 # 10
 
+# class CrawlerRecursive(Crawler):
+#     """
+#     Recursive implementation.
 
-class CrawlerRecursive(Crawler):
-    """
-    Recursive implementation.
+#     Get one URL of the title page and find requested number of articles recursively.
+#     """
 
-    Get one URL of the title page and find requested number of articles recursively.
-    """
+#     def __init__(self, config: Config) -> None:
+#         """
+#         Initialize an instance of the CrawlerRecursive class.
 
-    def __init__(self, config: Config) -> None:
-        """
-        Initialize an instance of the CrawlerRecursive class.
+#         Args:
+#             config (Config): Configuration
+#         """
+#         super().__init__(config)
 
-        Args:
-            config (Config): Configuration
-        """
-        super().__init__(config)
-
-    def find_articles(self) -> None:
-        """
-        Find number of article urls requested.
-        """
-        pass
+#     def find_articles(self) -> None:
+#         """
+#         Find number of article urls requested.
+#         """
 
 
 # 4, 6, 8, 10
