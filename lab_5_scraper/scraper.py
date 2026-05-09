a4287e6b373d63c20084098c1cc0b0a4cd59dac2
julia <juliamixi@mail.ru>
2026-05-10 02:02:51 +0300

    fix CI

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -20,6 +20,7 @@ from core_utils.article.io import to_meta, to_raw
 from core_utils.config_dto import ConfigDTO
 from core_utils.constants import ASSETS_PATH, CRAWLER_CONFIG_PATH
 
+
 class IncorrectSeedURLError(Exception):
     """
     Seed URL does not match standard pattern.
