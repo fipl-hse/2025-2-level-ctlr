760f3bed7b17125ae4c80486263250ef573e18f6
Teimur1 <teimurmolodoybrodyaga@gmail.com>
2026-05-10 22:28:00 +0300

    Made your changes, docstrings are back. If it passes tests - I'm done

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -57,8 +57,14 @@ class IncorrectVerifyError(Exception):
     """
 
 class Config:
+    """
+    Class for unpacking and validating configurations.
+    """
+
     def __init__(self, path_to_config: pathlib.Path) -> None:
         """
+        Initialize an instance of the Config class.
+
         Args:
             path_to_config (pathlib.Path): Path to configuration.
         """
@@ -76,6 +82,8 @@ class Config:
 
     def _extract_config_content(self) -> ConfigDTO:
         """
+        Get config values.
+
         Returns:
             ConfigDTO: Config values
         """
@@ -120,6 +128,8 @@ class Config:
 
     def get_seed_urls(self) -> list[str]:
         """
+        Retrieve seed urls.
+
         Returns:
             list[str]: Seed urls
         """
@@ -127,6 +137,8 @@ class Config:
 
     def get_num_articles(self) -> int:
         """
+        Retrieve total number of articles to scrape.
+
         Returns:
             int: Total number of articles to scrape
         """
@@ -134,6 +146,8 @@ class Config:
 
     def get_headers(self) -> dict[str, str]:
         """
+        Retrieve headers to use during requesting.
+
         Returns:
             dict[str, str]: Headers
         """
@@ -141,6 +155,8 @@ class Config:
 
     def get_encoding(self) -> str:
         """
+        Retrieve encoding to use during parsing.
+
         Returns:
             str: Encoding
         """
@@ -148,6 +164,8 @@ class Config:
 
     def get_timeout(self) -> int:
         """
+        Retrieve number of seconds to wait for response.
+
         Returns:
             int: Number of seconds to wait for response
         """
@@ -155,6 +173,8 @@ class Config:
 
     def get_verify_certificate(self) -> bool:
         """
+        Retrieve whether to verify certificate.
+
         Returns:
             bool: Whether to verify certificate or not
         """
@@ -162,29 +182,50 @@ class Config:
 
     def get_headless_mode(self) -> bool:
         """
-        Returns:
-            bool: Whether to use headless mode or not
+    Deliver a response from a request with given configuration.
+
+    Args:
+        url (str): Site url
+        config (Config): Configuration
+
+    Returns:
+        requests.models.Response: A response from a request
         """
         return self._headless_mode
 
 def make_request(url: str, config: Config) -> requests.models.Response:
     """
+    Deliver a response from a request with given configuration.
+
+    Args:
+        url (str): Site url
+        config (Config): Configuration
+
     Returns:
         requests.models.Response: A response from a request
     """
-    response = requests.get(
-        url,
-        headers=config.get_headers(),
-        timeout=config.get_timeout(),
-        verify=config.get_verify_certificate()
-    )
-    response.encoding = config.get_encoding()
-    return response
+    try:
+        response = requests.get(
+            url,
+            headers=config.get_headers(),
+            timeout=config.get_timeout(),
+            verify=config.get_verify_certificate()
+        )
+        response.encoding = config.get_encoding()
+        return response
+    except (requests.exceptions.RequestException, requests.exceptions.HTTPError):
+        return None
 
 
 class Crawler:
+    """
+    Crawler implementation.
+    """
+
     def __init__(self, config: Config) -> None:
         """
+        Initialize an instance of the Crawler class.
+
         Args:
             config (Config): Configuration
         """
@@ -193,6 +234,11 @@ class Crawler:
 
     def _extract_url(self, article_bs: Tag) -> str:
         """
+        Find and retrieve url from HTML.
+
+        Args:
+            article_bs (bs4.Tag): Tag instance
+
         Returns:
             str: Url from HTML
         """
@@ -204,13 +250,9 @@ class Crawler:
         Find articles.
         """
         for seed_url in self.get_search_urls():
-            try:
-                response = make_request(seed_url, self.config)
-                time.sleep(random.uniform(1, 2))
-            except requests.exceptions.RequestException:
-                continue
-
-            if not response.ok:
+            response = make_request(seed_url, self.config)
+            
+            if not response or not response.ok:
                 continue
 
             page_soup = BeautifulSoup(response.text, "lxml")
@@ -225,9 +267,13 @@ class Crawler:
 
                 if len(self.urls) >= self.config.get_num_articles():
                     return
+            
+            time.sleep(random.uniform(1, 2))
 
     def get_search_urls(self) -> list:
         """
+        Get seed_urls param.
+
         Returns:
             list: seed_urls param
         """
@@ -235,10 +281,16 @@ class Crawler:
 
 
 class HTMLParser:
+    """
+    HTMLParser implementation.
+    """
+
     def __init__(self, full_url: str, article_id: int, config: Config) -> None:
         """
+        Initialize an instance of the HTMLParser class.
+
         Args:
-            full_url (str): Article URL
+            full_url (str): Site url
             article_id (int): Article id
             config (Config): Configuration
         """
@@ -249,6 +301,8 @@ class HTMLParser:
 
     def _fill_article_with_text(self, article_soup: BeautifulSoup) -> None:
         """
+        Find text of article.
+
         Args:
             article_soup (bs4.BeautifulSoup): BeautifulSoup instance
         """
@@ -263,6 +317,8 @@ class HTMLParser:
 
     def _fill_article_with_meta_information(self, article_soup: BeautifulSoup) -> None:
         """
+        Find meta information of article.
+
         Args:
             article_soup (bs4.BeautifulSoup): BeautifulSoup instance
         """
@@ -270,21 +326,25 @@ class HTMLParser:
 
     def unify_date_format(self, date_str: str) -> datetime.datetime:
         """
+        Unify date format.
+
+        Args:
+            date_str (str): Date in text format
+
         Returns:
             datetime.datetime: Datetime object
         """
 
     def parse(self) -> Article | bool:
         """
+        Parse each article.
+
         Returns:
             Article | bool: Article instance, False in case of request error
         """
-        try:
-            response = make_request(self.full_url, self.config)
-        except requests.exceptions.RequestException:
-            return False
+        response = make_request(self.full_url, self.config)
 
-        if response.status_code != 200:
+        if not response or response.status_code != 200:
             return False
 
         article_soup = BeautifulSoup(response.text, "lxml")
@@ -296,6 +356,8 @@ class HTMLParser:
 
 def prepare_environment(base_path: pathlib.Path | str) -> None:
     """
+    Create ASSETS_PATH folder if no created and remove existing folder.
+
     Args:
         base_path (pathlib.Path | str): Path where articles stores
     """
