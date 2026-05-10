8f6386ed8b7a1262db9e7499e3577679cc67a1de
SletA <sletanimatics@gmail.com>
2026-05-10 15:34:29 +0300

    newCommitInOn1919

--- lab_5_scraper/scraper.py
+++ lab_5_scraper/scraper.py
@@ -297,7 +297,7 @@ class CrawlerRecursive(Crawler):
         Args:
             config (Config): Configuration
         """
-        super().__init__(config)
+        # super().__init__(config)
 
     def find_articles(self) -> None:
         """
