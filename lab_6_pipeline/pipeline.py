f58f7d8bcfde1ad74ea550d42087c0996f027a86
AMaryutina <anna151206maryutina@mail.ru>
2026-06-07 14:24:59 +0300

    fix CI

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -4,8 +4,6 @@ Pipeline for CONLL-U formatting.
 
 # pylint: disable=too-few-public-methods, unused-import, undefined-variable, too-many-nested-blocks, duplicate-code
 import pathlib
-from typing import cast
-
 import spacy_udpipe
 from spacy_conll import ConllParser
 
